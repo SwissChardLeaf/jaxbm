9794550b8f6ac01c44021db600ebee1afa82c199
SwissChardLeaf <research22426@gmail.com>
2026-04-27 11:04:22 -0700

    added ci testing

--- src/jax_bm/__init__.py
+++ src/jax_bm/__init__.py
@@ -2,6 +2,6 @@
 
 from jax_bm.bm import BoltzmannMachine, RestrictedBoltzmannMachine
 
-__all__ = ["BoltzmannMachine", "RestrictedBoltzmannMachine"]
+__all__ = ["BoltzmannMachine", "RestrictedBoltzmannMachine", "sample_single_chain", "sample_multiple_chains"]
 
 __version__ = "0.0.1"
